fb0fc68650c7a1e2d6982da331c65310a29c83e2
Guchuan.Jiang <jiangguchuan@huoyunren.com>
2015-05-07 23:03:17 +0800

    1

--- app/src/main/java/iebicycle/android/com/iebicycle/SplashActivity.java
+++ app/src/main/java/iebicycle/android/com/iebicycle/SplashActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationSet;
 import android.view.animation.TranslateAnimation;
 import android.widget.LinearLayout.LayoutParams;
 
@@ -24,26 +26,29 @@ public class SplashActivity extends Activity {
 
         Animation animation = new AlphaAnimation(0.1f, 1.0f);
         animation.setDuration(2000);
-        animation.setAnimationListener(new AnimationListener() {
-
-            @Override
-            public void onAnimationStart(Animation animation) {
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                View loginView = new LoginView(SplashActivity.this);
-                addContentView(loginView, new LayoutParams(LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-                Animation translateAnimation = new TranslateAnimation(0, 0,
-                        -300 * CommonUtils.getScreenDensity(SplashActivity.this), 0);
-                translateAnimation.setDuration(1000);
-                loginView.setAnimation(translateAnimation);
-            }
-        });
+        animation.setInterpolator(new AccelerateInterpolator());
+        animation.setAnimationListener(mSplashAnimationListener);
         view.setAnimation(animation);
     }
+
+    private AnimationListener mSplashAnimationListener = new AnimationListener() {
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            View loginView = new LoginView(SplashActivity.this);
+            addContentView(loginView, new LayoutParams(LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            Animation translateAnimation = new TranslateAnimation(0, 0,
+                    -400 * CommonUtils.getScreenDensity(SplashActivity.this), 0);
+            translateAnimation.setDuration(1000);
+            loginView.setAnimation(translateAnimation);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+        }
+    };
 }
